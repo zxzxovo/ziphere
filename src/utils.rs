41aaf7cafd04509e324da1f34a79f3787929108b
zhixiaLau <zhixiaovo@gmail.com>
2025-06-11 16:38:57 +0800

    util: add helper funcs in utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,28 @@
+//! The mod contains a series of helper function
+//! that will be used in other mod.
+
+use std::os::linux::fs::MetadataExt;
+use crate::error::Error::IOError;
+use crate::error::Result;
+use std::path::Path;
+use walkdir::WalkDir;
+use crate::error::IsIOError::{OnError, StdIoError, WalkError};
+
+
+/// Calculate the size of the passing path.
+pub(crate) fn calculate_size(path: &Path) -> Result<u64> {
+    if path.is_file() {
+        Ok(std::fs::metadata(path).map_err(|e| IOError(StdIoError(e)))?.len() )
+    } else if path.is_dir() {
+        let mut total_size = 0u64;
+        for entry in WalkDir::new(path) {
+            let entry = entry.map_err(|e| IOError(WalkError(e)) )?;
+            if entry.file_type().is_file() {
+                total_size += entry.metadata().map_err(|e| IOError(WalkError(e)) )?.len();
+            } 
+        }
+        Ok(total_size)
+    } else {
+        Err(IOError(OnError("Path is not a file".to_string())))
+    }
+}
