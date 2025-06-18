da12e3c5b55ba035490bdaba38627714c5e22ec3
zhixiaLau <zhixiaovo@gmail.com>
2025-06-18 16:09:37 +0800

    feat(7z-compress): complete 7z compress methods.

---
+++ src/formats.rs
@@ -0,0 +1,24 @@
+//! Supported formats and corresponding functions 
+//! and configs for `compress` and `decompress`.
+
+#[cfg(feature = "zip")]
+pub mod zip;
+#[cfg(feature = "sevenz")]
+pub mod sevenz;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Formats {
+    Zip,
+    SevenZ,
+}
+
+/// Compression level options.
+#[derive(Clone, Copy)]
+pub enum CompressLevel {
+    Fast,
+    Balanced,
+    Best,
+    Custom(u32)
+}
+
+
--- src/formats/sevenz.rs
+++ src/formats/sevenz.rs
@@ -1,7 +1,9 @@
 //! `.7z` format support.
 
+use std::path::PathBuf;
+use sevenz_rust2::SevenZMethodConfiguration;
 use crate::comde;
-use crate::comde::CompressStats;
+use crate::comde::{CompressStats, DecompressStats};
 use crate::error::Error::{CompressError, IOError};
 use crate::error::IsIOError::{OnError, StdIoError};
 use crate::utils::calculate_size;
@@ -13,29 +15,31 @@ impl comde::Compress for SevenzCompressor {
 
     fn compress_file(
         &self,
-        input: std::path::PathBuf,
-        output: std::path::PathBuf,
+        input: PathBuf,
+        output: PathBuf,
         config: Self::Configs
     ) -> crate::Result<CompressStats> {
         if !input.exists() {
             return Err(IOError(OnError("Path not exist.".to_string() )))
         }
         
+        // Start timing.
         let start_time = std::time::Instant::now();
         let origin_size = calculate_size(&input)?;
+        let mut writer = sevenz_rust2::SevenZWriter::create(&output)
+            .map_err(|e| CompressError(e.to_string()))?;
         
-        match config.password {
-            None => {
-                sevenz_rust2::compress_to_path(&input, &output).map_err(|e|
-                CompressError(e.to_string() ))?
-            }
-            Some(passwd) => {
-                sevenz_rust2::compress_to_path_encrypted(&input, &output, passwd.as_str().into()).map_err(|e| {
-                    CompressError(e.to_string())
-                })?
-            }
-        }
+        writer.set_content_methods(config.methods);
         
+        // Do solid_compress if enable.
+        if !config.solid_compress {
+            writer.push_source_path_non_solid(&input, |_| true)
+        } else {
+            writer.push_source_path(&input, |_| true)
+        }.map_err(|e| CompressError(e.to_string() ))?;
+        
+        // Write and stop timing.
+        writer.finish().map_err(|e| CompressError(e.to_string()))?;
         let compressed_size = std::fs::metadata(output).map_err(|e|
             IOError(StdIoError(e))
         )?.len();
@@ -45,22 +49,68 @@ impl comde::Compress for SevenzCompressor {
     }
 }
 
+/// The compression configuration of 7z.
 pub struct SevenzConfigs {
-    password: Option<String>,
+    solid_compress: bool,
+    methods: Vec<SevenZMethodConfiguration>
 }
 
 impl comde::ComdeConfig for SevenzConfigs {
     fn new() -> SevenzConfigs {
         SevenzConfigs {
-            password: None,
+            solid_compress: false,
+            methods: Vec::new()
         }
+    }
+
+    fn build(self) -> crate::Result<Self> {
+        Ok(self)
     }
 }
 
 impl SevenzConfigs {
+    /// Encrypt with AES256.
     pub fn set_password(mut self, password: &str) -> Self {
-        self.password = Some(password.to_string());
+        self.methods.push(sevenz_rust2::AesEncoderOptions::new(password.into()).into());
+        self
+    }
+    
+    /// Call to enable solid compression.
+    pub fn enable_solid_compress(mut self) -> Self {
+        self.solid_compress = true;
         self
     }
 
+    /// Use LZMA2 with default arguments.
+    pub fn use_lzma2(mut self) -> Self {
+        let m = sevenz_rust2::lzma::LZMA2Options::default();
+        self.methods.push(m.into());
+        self
+    }
+
+
+    /// Use LZMA2 with default arguments.
+    pub fn use_lzma2_with_preset(mut self, preset: u32) -> Self {
+        let m = sevenz_rust2::lzma::LZMA2Options::with_preset(preset);
+        self.methods.push(m.into());
+        self
+    }
+
+    /// Use ZStandard with given compression level.
+    #[cfg(feature = "sevenz-zstd")]
+    pub fn use_zstd_with_level(mut self, level: u32) -> Self {
+        let m = sevenz_rust2::ZStandardOptions::from_level(level);
+        self.methods.push(m.into());
+        self
+    }
+
+}
+
+
+impl comde::Decompress for SevenzCompressor {
+    type Configs = SevenzConfigs;
+
+    fn decompress_file(input: PathBuf, output: PathBuf, config: Self::Configs) -> crate::Result<DecompressStats> {
+        todo!()
+    }
 }
